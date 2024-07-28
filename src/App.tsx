7fa53e7660fa7e03abc32cee11ea4ce54d28f387
Alex Legerov <legerov13@gmail.com>
2024-07-28 13:08:30 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import { Button } from './components/Button/Button'
-import './styles/main.scss'
-
-
+import { useState } from "react";
+import { Button } from "./components/Button/Button";
+import "./styles/main.scss";
 
 function App() {
-  const [text, setText] = useState("asd")
+  const [text, setText] = useState("asd");
 
-   function handleclick() {
-    setText("SUKA")
-   }
-
+  function handleclick() {
+    setText("SUKA");
+    console.log("s");
+  }
 
   return (
     <>
-    <Button size='big'  onClick={handleclick} >{text}</Button>
+      <Button size="big" onClick={handleclick}>
+        {text}
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
